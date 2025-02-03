ffa498ca84d8cd769e1e32e1cb4feb1ac425b9f2
Artem Cherenkov <a.cherenkov@cian.ru>
2025-02-03 23:11:37 +0700

    добавил заготовку для дашборда

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
 
-  const email = session ? session.user?.email : undefined;
-
-  return (
-    <div>
-      <h1 className="text-2xl">АВС Просмотр</h1>
-      <h2 className="text-xl mt-4">
-        Веб-приложение для просмотра и хранения смет.
-      </h2>
-
-      {email ? <p>Вы авторизованы как {email}</p> : <p>Вы не авторизованы</p>}
-
-      <Link href="/dashboard" className="btn mt-10">
-        Перейти к дашборду
-      </Link>
-    </div>
-  );
+  if (session) {
+    return redirect("/dashboard");
+  } else {
+    return redirect("/sign-in");
+  }
 }
